1a32963f05e0765740234f61df38b96740a8bc0d
RublerubitZ <role0606@naver.com>
2026-05-20 11:37:09 +0900

    feat(frontend): ExploreNav 의 공지 탭 라우팅을 /notices 로 변경

---
+++ frontend/apps/web/app/notices/page.tsx
@@ -0,0 +1,9 @@
+// TODO: Implement notices feed page
+export default function NoticesPage() {
+  return (
+    <div>
+      <h1>공지사항</h1>
+      {/* Notice feed will be implemented in P3 Task 8 */}
+    </div>
+  );
+}
