d8977aa22333f0276c96f1da618cb112737b4555
RublerubitZ <role0606@naver.com>
2026-06-05 22:55:44 +0900

    feat(backend): FileException (UploadSizeExceeded / UnsupportedFileType)

---
+++ backend/src/main/java/com/duing/global/file/exception/FileException.java
@@ -0,0 +1,25 @@
+package com.duing.global.file.exception;
+
+import com.duing.global.exception.ApplicationException;
+import org.springframework.http.HttpStatus;
+
+public class FileException extends ApplicationException {
+
+    protected FileException(String message, HttpStatus status) {
+        super(message, status);
+    }
+
+    public static class UploadSizeExceededException extends FileException {
+        private static final String MESSAGE = "이미지 크기는 5MB 이하여야 합니다.";
+        public UploadSizeExceededException() {
+            super(MESSAGE, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public static class UnsupportedFileTypeException extends FileException {
+        private static final String MESSAGE = "지원하지 않는 이미지 형식입니다. (JPG, PNG, WEBP만 가능)";
+        public UnsupportedFileTypeException() {
+            super(MESSAGE, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
